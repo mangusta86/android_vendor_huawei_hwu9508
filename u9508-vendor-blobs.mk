a240aef5ad4552abf1ed407b151ca56181443483
Marco Fassi <marco@marco-Aspire-5742G.(none)>
2014-04-10 11:51:11 +0100

    test camera

--- u9508-vendor-blobs.mk
+++ u9508-vendor-blobs.mk
@@ -20,21 +20,60 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/lib/egl/libEGL_VIVANTE.so:system/lib/egl/libEGL_VIVANTE.so \
     $(LOCAL_PATH)/proprietary/system/lib/egl/libGLESv1_CM_VIVANTE.so:system/lib/egl/libGLESv1_CM_VIVANTE.so \
     $(LOCAL_PATH)/proprietary/system/lib/egl/libGLESv2_VIVANTE.so:system/lib/egl/libGLESv2_VIVANTE.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libGAL.so:system/lib/libGAL.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/copybit.k3v2oem1.so:system/lib/hw/copybit.k3v2oem1.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/gralloc.k3v2oem1.so:system/lib/hw/gralloc.k3v2oem1.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/hwcomposer.k3v2oem1.so:system/lib/hw/hwcomposer.k3v2oem1.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libsurfaceflinger.so:system/lib/libsurfaceflinger.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libsurfaceflinger_client.so:system/lib/libsurfaceflinger_client.so \
+    $(LOCAL_PATH)/proprietary/system/lib/liboverlay.so:system/lib/liboverlay.so \
+    $(LOCAL_PATH)/proprietary/system/bin/surfaceflinger:system/bin/surfaceflinger \
+    $(LOCAL_PATH)/proprietary/system/bin/mediaserver:system/bin/mediaserver \
+    $(LOCAL_PATH)/proprietary/system/bin/orientationd:system/bin/orientationd \
+    $(LOCAL_PATH)/proprietary/system/lib/libmedia.so:system/lib/libmedia.so \
     $(LOCAL_PATH)/proprietary/system/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
     $(LOCAL_PATH)/proprietary/system/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default.so \
     $(LOCAL_PATH)/proprietary/system/lib/hw/audio.primary.default.so:system/lib/hw/audio.primary.default.so \
     $(LOCAL_PATH)/proprietary/system/lib/hw/audio.primary.k3v2oem1.so:system/lib/hw/audio.primary.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/camera.k3v2oem1.so:system/lib/hw/camera.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.k3v2oem1.so:system/lib/hw/gps.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/copybit.k3v2oem1.so:system/lib/hw/copybit.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/gralloc.k3v2oem1.so:system/lib/hw/gralloc.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/hwcomposer.k3v2oem1.so:system/lib/hw/hwcomposer.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/sensors.k3v2oem1.so:system/lib/hw/sensors.k3v2oem1.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libmedia.so:system/lib/libmedia.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libgui.so:system/lib/libgui.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libGAL.so:system/lib/libGAL.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libGLSLC.so:system/lib/libGLSLC.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libaudio-resampler.so:system/lib/libaudio-resampler.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libaudioflinger.so:system/lib/libaudioflinger.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libdolbyaudioeffectnativeservice.so:system/lib/libdolbyaudioeffectnativeservice.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libdolbymobileaudioeffect_jni.so:system/lib/libdolbymobileaudioeffect_jni.so \
     $(LOCAL_PATH)/proprietary/system/lib/libMirrorAudioService.so:system/lib/libMirrorAudioService.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libmirror_media.so:system/lib/libmirror_media.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/libbcmfm_if.so:system/lib/hw/libbcmfm_if.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/camera.k3v2oem1.so:system/lib/hw/camera.k3v2oem1.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libcamera_core.so:system/lib/libcamera_core.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libcamera_scalado.so:system/lib/libcamera_scalado.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libimageinfo.so:system/lib/libimageinfo.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libgnuexif.so:system/lib/libgnuexif.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libskia.so:system/lib/libskia.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libhelixplayer.so:system/lib/libhelixplayer.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/sensors.k3v2oem1.so:system/lib/hw/sensors.k3v2oem1.so \
+    $(LOCAL_PATH)/proprietary/system/bin/akm8975:system/bin/akm8975 \
+    $(LOCAL_PATH)/proprietary/system/bin/akmd8963:system/bin/akmd8963 \
+    $(LOCAL_PATH)/proprietary/system/bin/geomagneticd:system/bin/geomagneticd \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.k3v2oem1.so:system/lib/hw/gps.k3v2oem1.so \
+    $(LOCAL_PATH)/proprietary/system/bin/glgps:system/bin/glgps \
+    $(LOCAL_PATH)/proprietary/system/bin/gpslogd:system/bin/gpslogd \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/bluetooth.default.so:system/lib/hw/bluetooth.default.so \
+    $(LOCAL_PATH)/proprietary/system/vendor/lib/libbt-vendor.so:system/vendor/lib/libbt-vendor.so \
+    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd.bin:system/vendor/firmware/fw_bcmdhd.bin \
+    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd_apsta.bin:system/vendor/firmware/fw_bcmdhd_apsta.bin \
+    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd_p2p.bin:system/vendor/firmware/fw_bcmdhd_p2p.bin \
+    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd_test.bin:system/vendor/firmware/fw_bcmdhd_test.bin \
+    $(LOCAL_PATH)/proprietary/system/vendor/firmware/nvram.txt:system/vendor/firmware/nvram.txt \
+    $(LOCAL_PATH)/proprietary/system/vendor/firmware/BCM4330.hcd:system/vendor/firmware/BCM4330.hcd \
+    $(LOCAL_PATH)/proprietary/system/lib/libril.so:system/lib/libril.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libxgold-ril.so:system/lib/libxgold-ril.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libhuawei-audio-ril.so:system/lib/libhuawei-audio-ril.so \
+    $(LOCAL_PATH)/proprietary/system/bin/rild:system/bin/rild \
+    $(LOCAL_PATH)/proprietary/system/bin/IMCdownload_App:system/bin/IMCdownload_App \
+    $(LOCAL_PATH)/proprietary/system/bin/Injection_nv:system/bin/Injection_nv \
+    $(LOCAL_PATH)/proprietary/system/bin/nvm_server:system/bin/nvm_server \
+    $(LOCAL_PATH)/proprietary/system/bin/imc_poweron:system/bin/imc_poweron \
+    $(LOCAL_PATH)/proprietary/system/lib/libGLSLC.so:system/lib/libGLSLC.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libgui.so:system/lib/libgui.so \
     $(LOCAL_PATH)/proprietary/system/lib/libRedEye.so:system/lib/libRedEye.so \
     $(LOCAL_PATH)/proprietary/system/lib/lib_8290.so:system/lib/lib_8290.so \
     $(LOCAL_PATH)/proprietary/system/lib/lib_g1.so:system/lib/lib_g1.so \
@@ -48,65 +87,31 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/lib/lib_k3_omx_vc1.so:system/lib/lib_k3_omx_vc1.so \
     $(LOCAL_PATH)/proprietary/system/lib/lib_k3_omx_vp8.so:system/lib/lib_k3_omx_vp8.so \
     $(LOCAL_PATH)/proprietary/system/lib/lib_k3_omxcore.so:system/lib/lib_k3_omxcore.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libaudioflinger.so:system/lib/libaudioflinger.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libcamera_core.so:system/lib/libcamera_core.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libcamera_scalado.so:system/lib/libcamera_scalado.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libisdb_omxcore.so:system/lib/libisdb_omxcore.so \
     $(LOCAL_PATH)/proprietary/system/lib/libclcore.bc:system/lib/libclcore.bc \
     $(LOCAL_PATH)/proprietary/system/lib/libclcore_neon.bc:system/lib/libclcore_neon.bc \
-    $(LOCAL_PATH)/proprietary/system/lib/libdolbyaudioeffectnativeservice.so:system/lib/libdolbyaudioeffectnativeservice.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libdolbymobileaudioeffect_jni.so:system/lib/libdolbymobileaudioeffect_jni.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libimageinfo.so:system/lib/libimageinfo.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libisdb_omxcore.so:system/lib/libisdb_omxcore.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libmirror_media.so:system/lib/libmirror_media.so \
     $(LOCAL_PATH)/proprietary/system/lib/libmrc_cg_postprocess.so:system/lib/libmrc_cg_postprocess.so \
-    $(LOCAL_PATH)/proprietary/system/lib/liboverlay.so:system/lib/liboverlay.so \
     $(LOCAL_PATH)/proprietary/system/lib/libstagefright_wfd.so:system/lib/libstagefright_wfd.so \
     $(LOCAL_PATH)/proprietary/system/lib/libstagefrighthw.so:system/lib/libstagefrighthw.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsurfaceflinger.so:system/lib/libsurfaceflinger.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsurfaceflinger_client.so:system/lib/libsurfaceflinger_client.so \
     $(LOCAL_PATH)/proprietary/system/lib/libui.so:system/lib/libui.so \
     $(LOCAL_PATH)/proprietary/system/lib/libvpp.so:system/lib/libvpp.so \
     $(LOCAL_PATH)/proprietary/system/lib/libwvm.so:system/lib/libwvm.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libgnuexif.so:system/lib/libgnuexif.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libril.so:system/lib/libril.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libxgold-ril.so:system/lib/libxgold-ril.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libhuawei-audio-ril.so:system/lib/libhuawei-audio-ril.so \
-    $(LOCAL_PATH)/proprietary/system/bin/rild:system/bin/rild \
-    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd.bin:system/vendor/firmware/fw_bcmdhd.bin \
-    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd_apsta.bin:system/vendor/firmware/fw_bcmdhd_apsta.bin \
-    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd_p2p.bin:system/vendor/firmware/fw_bcmdhd_p2p.bin \
-    $(LOCAL_PATH)/proprietary/system/vendor/firmware/fw_bcmdhd_test.bin:system/vendor/firmware/fw_bcmdhd_test.bin \
-    $(LOCAL_PATH)/proprietary/system/vendor/firmware/nvram.txt:system/vendor/firmware/nvram.txt \
-    $(LOCAL_PATH)/proprietary/system/vendor/firmware/BCM4330.hcd:system/vendor/firmware/BCM4330.hcd \
-    $(LOCAL_PATH)/proprietary/system/bin/IMCdownload_App:system/bin/IMCdownload_App \
-    $(LOCAL_PATH)/proprietary/system/bin/Injection_nv:system/bin/Injection_nv \
-    $(LOCAL_PATH)/proprietary/system/bin/akm8975:system/bin/akm8975 \
-    $(LOCAL_PATH)/proprietary/system/bin/akmd8963:system/bin/akmd8963 \
     $(LOCAL_PATH)/proprietary/system/bin/am:system/bin/am \
     $(LOCAL_PATH)/proprietary/system/bin/bmgr:system/bin/bmgr \
     $(LOCAL_PATH)/proprietary/system/bin/bu:system/bin/bu \
     $(LOCAL_PATH)/proprietary/system/bin/content:system/bin/content \
-    $(LOCAL_PATH)/proprietary/system/bin/geomagneticd:system/bin/geomagneticd \
-    $(LOCAL_PATH)/proprietary/system/bin/glgps:system/bin/glgps \
-    $(LOCAL_PATH)/proprietary/system/bin/gpslogd:system/bin/gpslogd \
     $(LOCAL_PATH)/proprietary/system/bin/hostapd:system/bin/hostapd \
-    $(LOCAL_PATH)/proprietary/system/bin/imc_poweron:system/bin/imc_poweron \
     $(LOCAL_PATH)/proprietary/system/bin/ime:system/bin/ime \
     $(LOCAL_PATH)/proprietary/system/bin/input:system/bin/input \
-    $(LOCAL_PATH)/proprietary/system/bin/mediaserver:system/bin/mediaserver \
     $(LOCAL_PATH)/proprietary/system/bin/monkey:system/bin/monkey \
-    $(LOCAL_PATH)/proprietary/system/bin/nvm_server:system/bin/nvm_server \
-    $(LOCAL_PATH)/proprietary/system/bin/orientationd:system/bin/orientationd \
     $(LOCAL_PATH)/proprietary/system/bin/pcscd:system/bin/pcscd \
     $(LOCAL_PATH)/proprietary/system/bin/pm:system/bin/pm \
     $(LOCAL_PATH)/proprietary/system/bin/requestsync:system/bin/requestsync \
     $(LOCAL_PATH)/proprietary/system/bin/settings:system/bin/settings \
-    $(LOCAL_PATH)/proprietary/system/bin/surfaceflinger:system/bin/surfaceflinger \
     $(LOCAL_PATH)/proprietary/system/bin/svc:system/bin/svc \
     $(LOCAL_PATH)/proprietary/system/bin/testpcsc:system/bin/testpcsc \
     $(LOCAL_PATH)/proprietary/system/bin/testpcsc_sendapdu:system/bin/testpcsc_sendapdu \
     $(LOCAL_PATH)/proprietary/system/bin/uiautomator:system/bin/uiautomator \
     $(LOCAL_PATH)/proprietary/system/bin/wl:system/bin/wl \
     $(LOCAL_PATH)/proprietary/system/isp.bin:system/isp.bin \
-    $(LOCAL_PATH)/proprietary/system/ons.bin:system/ons.bin \
-    $(LOCAL_PATH)/proprietary/system/vendor/lib/libbt-vendor.so:system/vendor/lib/libbt-vendor.so
+    $(LOCAL_PATH)/proprietary/system/ons.bin:system/ons.bin
